03180dc5be2d9d6dc6d54db016fa8cb180519a8a
Adrian Hitz <32393038+adrianhitz@users.noreply.github.com>
2019-06-09 21:32:00 +0200

    Add basic command handling

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,9 @@ import qualified Data.Text.IO as TIO
 
 import Discord
 
+utherQuote :: T.Text
+utherQuote = "You are not my king yet, boy! Nor would I obey that command even if you were!"
+
 main :: IO ()
 main = do
     -- Causes text to printed to the console immediately instead of when the program terminates
@@ -23,13 +26,19 @@ main = do
 
 echo :: (RestChan, Gateway, z) -> IO ()
 echo dis = do
-    e <- nextEvent dis
-    case e of
+    event <- nextEvent dis
+    case event of
         Left er -> putStrLn ("Event error: " <> show er)
         Right (MessageCreate message) -> do
-            when (not (fromBot message)) $ do
-                resp <- restCall dis (CreateMessage (messageChannel message) (messageText message))
+            when ((not (fromBot message)) && (msgIsCommand message)) $ do
+                -- Log command sent by user to console
                 putStrLn ((authorHandle message) ++ ": " ++ (show (messageText message)))
+                -- Send response
+                resp <- (restCall dis (CreateMessage (messageChannel message) utherQuote))
+                -- Log response to console
+                case resp of
+                    Left error -> putStrLn ("REST error: " <> show error)
+                    Right message -> putStrLn ("fprod-bot: " ++ (show (messageText message)))
             echo dis
         _ -> echo dis
 
@@ -45,6 +54,12 @@ authorHandle message = userHandle (messageAuthor message)
 userHandle :: User -> String
 userHandle user = (userName user) ++ "#" ++ (userDiscrim user)
 
--- | Returns true if the given message is a bot command (starts with '!' followed by at least one other character)
+-- | Returns true if the given string is a bot command (starts with '!' followed by at least one other non-space character)
 isCommand :: String -> Bool
-isCommand (c:cs) = c == '!' && cs /= []
+isCommand [] = False
+isCommand [c] = False
+isCommand (c:cs) = (c == '!') && (head cs) /= ' '
+
+-- | Returns true if the given message is a bot command
+msgIsCommand :: Message -> Bool
+msgIsCommand message = isCommand (T.unpack (messageText message))
